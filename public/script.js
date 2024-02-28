849c3e3f355a7f10bd19580922bdfe9096c3df5d
Gaurav <gauravyadav7403@student.sfit.ac.in>
2024-02-28 23:17:19 +0530

    code changed and image generator is working

---
+++ public/script.js
@@ -0,0 +1,29 @@
+document.getElementById('textForm').addEventListener('submit', async (e) => {
+      e.preventDefault();
+      const textInput = document.getElementById('textInput').value;
+      const generatedImage = document.getElementById('generatedImage');
+      
+      try {
+          const response = await fetch('https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0', {
+              method: 'POST',
+              headers: {
+                  'Authorization': 'Bearer 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',
+                  'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({
+                  inputs: textInput
+              })
+          });
+          
+          if (!response.ok) {
+              throw new Error('Network response was not ok');
+          }
+          
+          const blob = await response.blob();
+          const imageUrl = URL.createObjectURL(blob);
+          generatedImage.src = imageUrl;
+      } catch (error) {
+          console.error('There was a problem with the fetch operation:', error);
+      }
+  });
+  
