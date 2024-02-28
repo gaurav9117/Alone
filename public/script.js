c2a1de18ce7eec0986b28f1accdee7b076398877
Gaurav <gauravyadav7403@student.sfit.ac.in>
2024-02-29 00:24:32 +0530

    added generator.js and updated script.js

--- public/script.js
+++ public/script.js
@@ -1,29 +1,26 @@
 document.getElementById('textForm').addEventListener('submit', async (e) => {
-      e.preventDefault();
-      const textInput = document.getElementById('textInput').value;
-      const generatedImage = document.getElementById('generatedImage');
-      
-      try {
-          const response = await fetch('https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0', {
-              method: 'POST',
-              headers: {
-                  'Authorization': 'Bearer tocken',
-                  'Content-Type': 'application/json'
-              },
-              body: JSON.stringify({
-                  inputs: textInput
-              })
-          });
-          
-          if (!response.ok) {
-              throw new Error('Network response was not ok');
-          }
-          
-          const blob = await response.blob();
-          const imageUrl = URL.createObjectURL(blob);
-          generatedImage.src = imageUrl;
-      } catch (error) {
-          console.error('There was a problem with the fetch operation:', error);
-      }
-  });
-  
+    e.preventDefault();
+    const textInput = document.getElementById('textInput').value;
+    const generatedImage = document.getElementById('generatedImage');
+    
+    try {
+        const response = await fetch('http://localhost:3001/generate', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                inputs: textInput
+            })
+        });
+        
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        
+        const data = await response.json();
+        generatedImage.src = data.imageUrl;
+    } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+    }
+});
